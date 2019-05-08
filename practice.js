1fa9dbb5c209225d1c4c8d59ad2e93cf56026446
Derek <Derek@Dereks-MacBook-Pro.local>
2019-05-08 14:29:38 -0600

    finished through 4

--- practice.js
+++ practice.js
@@ -62,13 +62,20 @@ favoriteThings.book = 'Harry Potter'
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack.
 */
 
-//Code here
+let backPack = {}
+
+var item = 'firstPocket'
+
+backPack['firstPocket'] = item
+
+backPack.firstPocket = 'chapstick'
+
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+backPack
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -76,7 +83,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console.
 */
 
-//Code here
+console.log(backPack)
 
 ////////// PROBLEM 4 //////////
 
@@ -97,7 +104,9 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2.name = 'Bryan G. Smith'
+user2.email = 'bryan.smith@devmounta.in'
+
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
