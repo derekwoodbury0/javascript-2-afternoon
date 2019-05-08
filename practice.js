bc2f7c42b8ef446acffeedd16baff8e9bf655298
Derek <Derek@Dereks-MacBook-Pro.local>
2019-05-08 14:04:24 -0600

    first change

--- practice.js
+++ practice.js
@@ -14,7 +14,12 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+let me = {
+  name: 'Derek',
+  age: 27
+}
+
+me.name
 
 ////////// PROBLEM 2 //////////
 
@@ -23,21 +28,29 @@
   Have the values to those keys be strings that are equal to your favorite thing in that category.
 */
 
-//Code here
-
+var favoriteThings = {
+  band: 'A Day To Remember',
+  food: '?',
+  person: 'Damian Lillard',
+  book: 'All The Light We Cannot See',
+  movie: 'The Last Samurai',
+  holiday: 'Christmas'
+}
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = 'GT-R'
+favoriteThings.brand = 'TWLOHA'
 
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = 'Chicken Nuggets'
+favoriteThings.book = 'Harry Potter'
 
 ////////// PROBLEM 3 //////////
 
